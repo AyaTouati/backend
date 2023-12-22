3952a67ad8621514b18de0ccde04559f5a867e20
AyaToua1 <ayatouati555@gmail.com>
2023-12-22 14:20:57 +0100

    version4

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express =require('express');
 const mongoose =require("mongoose");
 const dotenv =require('dotenv');
-const app = express();
 const categorieRouter =require("./routes/categorie.route")
 const scategorieRouter =require("./routes/scategorie.route")
 const articleRouter =require("./routes/article.route")
+const cors=require('cors')
+const app= express();
+app.use(cors())
 dotenv.config()
 app.use(express.json());
 
